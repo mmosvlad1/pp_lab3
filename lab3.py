1f379f41cce482d2c13a927b0f49ed30626fa1f7
mmosvlad <113801483+mmosvlad1@users.noreply.github.com>
2023-10-21 21:57:57 +0300

    final version

--- lab3.py
+++ lab3.py
@@ -8,5 +8,4 @@ def hello_world():
 if __name__ == '__main__':
     app.run(debug=True)
 
-#curl -v -XGET http://127.0.0.1:8000/api/v1/hello-world-21
 #curl -v -XGET http://127.0.0.1:5000/api/v1/hello-world-21
